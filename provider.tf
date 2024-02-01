cc73eff195633f78357669ddfc6625975ef5e381
Rohitvarma123 <128877028+Rohitvarma123@users.noreply.github.com>
2024-02-01 11:35:46 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   # Configuration options
-  region = "us-east-2"
+  region = "eu-west-1"
   access_key =  "AKIARGE4PYVDWY72FQWH"
   secret_key = "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"
 }
